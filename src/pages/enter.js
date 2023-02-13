1235f79ad2145b956bd6fbf6898c15a7b6d323be
mfaizk <faizk123485@gmail.com>
2023-02-13 20:25:02 +0530

    initial component introduce

---
+++ src/pages/enter.js
@@ -0,0 +1,7 @@
+export default function EnterPage({}) {
+  return (
+    <main>
+      <h1> Signup Page</h1>
+    </main>
+  );
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,13 +2,23 @@ import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "@next/font/google";
 import styles from "@/styles/Home.module.css";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <div>
-      <h1>Hello</h1>
+      <Link
+        prefetch={false}
+        href={{
+          pathname: "/[username]",
+          query: { username: "jeffd23" },
+        }}
+      >
+        {" "}
+        <a>Jeffs profile</a>
+      </Link>
     </div>
   );
 }
